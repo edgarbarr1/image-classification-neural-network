698a98d3a68609ef0c913ce8a536814abe500fa0
Edgar Barron <edgarraul98@icloud.com>
2021-06-28 07:58:31 -0500

    up_to_date_push

---
+++ src/functions.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow import keras
+from tensorflow.keras import layers
+import os
+import cv2
+import random
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+from keras import Sequential
+from keras.layers import Dense
+from sklearn.metrics import confusion_matrix
+from keras.metrics import Recall, Precision
+from keras.layers import Conv2D, MaxPooling2D, Flatten, InputLayer
+from sklearn.metrics import plot_confusion_matrix
+import time
+
+
+def visualize_training_results(history, iteration):
+    '''
+    From https://machinelearningmastery.com/display-deep-learning-model-training-history-in-keras/
+    
+    Input: keras history object (output from trained model)
+    '''
+    fig, (ax1, ax2, ax3) = plt.subplots(3, sharex=True)
+    fig.suptitle('Model Results')
+
+    # summarize history for accuracy
+    ax1.plot(history.history['recall_{}'.format(iteration)])
+    ax1.plot(history.history['val_recall_{}'.format(iteration)])
+    ax1.set_ylabel('Recall')
+    ax1.legend(['train', 'test'], loc='upper left')
+    # summarize history for loss
+    ax2.plot(history.history['loss'])
+    ax2.plot(history.history['val_loss'])
+    ax2.set_ylabel('Loss')
+    ax2.legend(['train', 'test'], loc='upper left')
+    
+    ax3.plot(history.history['precision_{}'.format(iteration)])
+    ax3.plot(history.history['val_precision_{}'.format(iteration)])
+    ax3.set_ylabel('Precision')
+    ax3.legend(['train', 'test'], loc='upper left')
+    
+    plt.xlabel('Epoch')
+    plt.show()
+    
+
+def create_training_data():
+    for category in categories:
+        path_train = os.path.join(data_dir_train,category)
+        class_num = categories.index(category)
+        for img in os.listdir(path_train):
+            try:
+                img_array_train = cv2.imread(os.path.join(path_train,img), cv2.IMREAD_GRAYSCALE)
+                train_array = cv2.resize(img_array_train,(img_size, img_size))
+                train_data.append([train_array, class_num])
+            except Exception as e:
+                pass
